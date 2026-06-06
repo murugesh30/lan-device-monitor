09ad0d64732599be1790a9591989545a2f7b9bb0
murugesh30 <mguddapur123@gmail.com>
2026-06-06 14:53:10 +0530

    remove genrated and test files

--- test_interfaces.py
+++
@@ -1,9 +0,0 @@
-import psutil
-
-for interface, addresses in psutil.net_if_addrs().items():
-    print("\nInterface:", interface)
-
-    for addr in addresses:
-        print("  Family:", addr.family)
-        print("  Address:", addr.address)
-        print("  Netmask:", addr.netmask)
